f86088708aa0787dfa8572315948ba1e26356e7f
bf0880-boop <bf0880@est.ort.edu.ar>
2025-09-04 11:18:24 -0300

    Terminé Wally

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import { generarEspacio } from "./busqueda.js";
 let lista= generarEspacio(100);
+
 console.log(lista);
 for(let i=0; i<lista.length; i++){
     let posicionlista= lista [i];
     if (posicionlista==="Wally"){
     console.log("Wally está en la posición " + i)
     }
+}
+console.log("Nombres que empiezan con W:");
+for (let i = 0; i < lista.length; i++) {
+  if (lista[i][0] === "W") {
+    console.log(lista[i] + " (posición " + i + ")");
+  }
+}
+
+console.log("Nombres que contienen 'll':");
+for (let i = 0; i < lista.length; i++) {
+  if (lista[i].includes("ll")) {
+    console.log(lista[i] + " (posición " + i + ")");
+  }
 }
